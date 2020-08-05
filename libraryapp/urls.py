4123f94a8d9def946d93811d415efe5cea2c7ce8
John Bain <johnknoxbain@gmail.com>
2020-08-05 14:56:17 -0500

    chapters 1-7

---
+++ libraryapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import include, path
+from .views import *
+from django.urls import path
+
+app_name = "libraryapp"
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('books/', book_list, name='books'),
+    path('books/<int:book_id>/', book_details, name='book'),
+    path('books/form', book_form, name='book_form'),
+    path('librarians/', list_librarians, name='librarians'),
+    path('librarians/<int:librarian_id>/', librarian_details, name='librarian_details'),
+    path('libraries/', library_list, name='libraries'),
+    path('libraries/<int:library_id>/', library_details, name='library_details'),
+    path('libraries/form', library_form, name='library_form'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout_user, name='logout'),
+]
---
+++ libraryapp/views/books/list.py
@@ -0,0 +1,56 @@
+import sqlite3
+from django.shortcuts import render
+from libraryapp.models import Book
+from ..connection import Connection
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, reverse
+from libraryapp.models import model_factory
+
+@login_required
+def book_list(request):
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = model_factory(Book)
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select
+                b.id,
+                b.title,
+                b.isbn_number,
+                b.author,
+                b.year_published,
+                b.librarian_id,
+                b.location_id
+            from libraryapp_book b
+            """)
+
+            all_books = db_cursor.fetchall()
+
+ 
+        template = 'books/list.html'
+        context = {
+            'all_books': all_books
+        }
+
+        return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO libraryapp_book
+            (
+                title, author, isbn_number,
+                year_published, location_id, librarian_id
+            )
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (form_data['title'], form_data['author'],
+                form_data['isbn_number'], form_data['year_published'],
+                form_data['location'], request.user.librarian.id))
+
+        return redirect(reverse('libraryapp:books'))
